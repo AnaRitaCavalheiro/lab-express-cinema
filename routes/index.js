f30c176d7b84e956c9f2edf49428c081cb102ed0
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-11-09 14:45:19 +0000

    not styled

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const movie = require('../models/Movie.model.js');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+   movie.find()
+   .then((alltheMoviesFromMongoDB) => {
+       res.render('movies', {movies: alltheMoviesFromMongoDB});
+   })
+   .catch((err) => {
+       res.render('error', {err});
+   })
+});
+
+router.get('/movies/:movieId', (req, res) => {
+let movieId = req.params.movieId;
+
+    movie.findById(movieId)
+    .then((theMovieFound) => {
+        res.render('movie-details', {movie: theMovieFound})
+    })
+    .catch((err) => {
+        res.render('error', {err});
+      })
+});
 
 module.exports = router;
